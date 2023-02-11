bb55f5cd82718d8637c1ba7c1930345e07e28f64
Jane <evgenia.kochukova@gmail.com>
2023-02-10 22:06:25 -0300

    New commit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,6 +1,11 @@
 public class CreditPaymentService {
 
-    public int calculate(int amount, double rate, double factor) {
+    public int calculate(int amount, double rate, int mes) {
+
+        double a = 1 + rate;
+        float factor;
+        factor = (float) Math.pow(a, mes);
+
         int payment = (int) (amount * ((rate * factor) / (factor - 1)));
 
 
--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,8 @@ public class Main {
         int mes = 36;
         int amount = 1_000_000;
         double rate = 9.99 / 12 / 100;
-        double a;
-        a = 1 + rate;
-        float factor;
-        factor = (float) Math.pow(a, mes);
 
-        int payment = service.calculate(amount, rate, factor);
+        int payment = service.calculate(amount, rate, mes);
 
 
         System.out.println(payment);
